fb6ec16517b065abf158b805fff56812c1a6dd29
Doug Day <dougrday@gmail.com>
2018-10-12 14:23:40 -0600

    Move go files into their own subfolder to begin work on C# side

--- src/go/engine/engine.go
+++ src/go/engine/engine.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/landmarkhw/profile-go-vs-c-sharp-for-microservices/person"
 	"github.com/landmarkhw/profile-go-vs-c-sharp-for-microservices/status"
+	"github.com/landmarkhw/profile-go-vs-c-sharp-for-microservices/test"
 )
 
 // Run the REST API web server
@@ -12,6 +13,7 @@ func Run() {
 
 	person.Routes(engine)
 	status.Routes(engine)
+	test.Routes(engine)
 
 	engine.Run(":5000")
 }
